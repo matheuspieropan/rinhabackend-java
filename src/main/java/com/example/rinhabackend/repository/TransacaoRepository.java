3ca9b42d1b2e0f6c24080237bc1cf0d57b918f5b
mamapisouza <Pi204326@>
2024-02-25 16:09:27 -0300

    nova abordagem para trabalhar com concorrência... teste

--- src/main/java/com/example/rinhabackend/repository/TransacaoRepository.java
+++ src/main/java/com/example/rinhabackend/repository/TransacaoRepository.java
@@ -2,36 +2,13 @@ package com.example.rinhabackend.repository;
 
 import com.example.rinhabackend.conexao.DatabaseConnection;
 import com.example.rinhabackend.dto.TransacaoResponse;
-import com.example.rinhabackend.exceptions.ValidacaoRequestException;
-import com.example.rinhabackend.model.Transacao;
 
-import java.sql.*;
-
-import static com.example.rinhabackend.enums.HttpStatus.UNPROCESSABLE_ENTITY;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class TransacaoRepository {
-
-    public TransacaoResponse atualizaSaldoERegistraTransacao(int idCliente, Transacao transacao) {
-        TransacaoResponse transacaoResponse = new TransacaoResponse();
-        try (Connection connection = DatabaseConnection.getDataSource().getConnection()) {
-            String procedureCall = "{call public.efetuar_transacao(?, ?, ?, ?)}";
-            CallableStatement callableStatement = connection.prepareCall(procedureCall);
-
-            callableStatement.setInt(1, idCliente);
-            callableStatement.setString(2, String.valueOf(transacao.getTipo()).toLowerCase());
-            callableStatement.setInt(3, transacao.getValor());
-            callableStatement.setString(4, transacao.getDescricao());
-
-            ResultSet resultSet = callableStatement.executeQuery();
-            if (resultSet.next()) {
-                transacaoResponse.setLimite(resultSet.getInt("limiteRetorno"));
-                transacaoResponse.setSaldo(resultSet.getInt("saldoRetorno"));
-            }
-        } catch (SQLException ex) {
-            throw new ValidacaoRequestException(UNPROCESSABLE_ENTITY.getCodigo(), "Transação ultrapassa valor limite");
-        }
-        return transacaoResponse;
-    }
 
     public TransacaoResponse realizarTransacao(int idCliente, char operacao, int valorOperacao, String descricao) {
         TransacaoResponse transacaoResponse = new TransacaoResponse();
